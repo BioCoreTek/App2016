e7f76e192ed5a4a8c8f44a2a84320a69adb271b9
Annie Perrier <annieperrier@users.noreply.github.com>
2016-10-17 23:23:49 -0400

    timer support: start, stop, pause, unpause.

---
+++ public/js/Timer.js
@@ -0,0 +1,117 @@
+////////////////////////////// TIMER //////////////////////////////////////////
+
+function Timer()
+{
+	this.counterEl = $(".counter");
+	// total time a game last in milliseconds - 30 minutes
+	this.totalTime = 30*60*1000;
+	// time the game started in milliseconds
+	this.startTime = null;
+	// time the game stopped in milliseconds
+	this.stopTime = null;
+	// the current time as per last run time in the interval in milliseconds
+	this.curTime = null;
+
+	// array of pause objects
+	// startTime - the start time of the pause in milliseconds
+	// stopTime - the start time of the pause in milliseconds
+	this.pauses = [];
+	// the current pause object if currently paused
+	this.currentPause = null;
+	// the total number of milliseconds the game was paused
+	this.pauseTotalTime = 0;
+
+	// the interval object
+	this.intTimer = null;
+};
+
+Timer.prototype.init = function()
+{
+	var self = this;
+
+	// listen for timer states
+	PubSub.subscribe('timer', function(msg, data)
+	{
+		debug.debug('Timer PubSub sub timer', msg, data);
+		self.processTimerEvent(data);
+	});
+};
+
+Timer.prototype.processTimerEvent = function(data)
+{
+	debug.debug('Timer processTimerEvent', data);
+	switch(data.event)
+	{
+		case 'start':
+			this.startTime = new Date().getTime();
+			this.startTimer();
+			break;
+		case 'pause':
+			this.pauseTimer();
+			break;
+		case 'unpause':
+			this.unpauseTimer();
+			break;
+		case 'stop':
+			this.stopTimer();
+			this.stopTime = this.curTime;
+			break;
+	}
+};
+
+Timer.prototype.drawTime = function()
+{
+	debug.log('Timer drawTime');
+	var elapsed = this.curTime - this.startTime - this.pauseTotalTime;
+	var timeLeft = this.totalTime - elapsed;
+	var minutes = Math.floor(timeLeft/(1000*60));
+	var r = timeLeft - (minutes*1000*60);
+	var seconds = Math.floor(r/1000);
+	if (minutes < 10)
+		minutes = '0'+minutes;
+	if (seconds < 10)
+		seconds = '0'+seconds;
+	this.counterEl.html(minutes+':'+seconds);
+};
+
+Timer.prototype.startTimer = function()
+{
+	debug.debug('Timer startTimer');
+
+	var self = this;
+
+	this.intTimer = setInterval(function()
+	{
+		self.curTime = new Date().getTime();
+		self.drawTime();
+	}, 500);
+};
+
+Timer.prototype.stopTimer = function()
+{
+	debug.debug('Timer stopTimer');
+
+	clearInterval(this.intTimer);
+};
+
+Timer.prototype.pauseTimer = function()
+{
+	debug.debug('Timer pauseTimer');
+	this.stopTimer();
+	var p = {
+		startTime: new Date().getTime(),
+		stopTime: null
+	};
+	this.pauses.push(p);
+	this.currentPause = this.pauses[this.pauses.length-1];
+};
+
+Timer.prototype.unpauseTimer = function()
+{
+	debug.debug('Timer unpauseTimer');
+	this.currentPause.stopTime = new Date().getTime();
+	var t = this.currentPause.stopTime - this.currentPause.startTime;
+	this.pauseTotalTime += t;
+	this.currentPause = null;
+	this.startTimer();
+};
--- public/js/script.js
+++ public/js/script.js
@@ -7,6 +7,7 @@ var section;
 var gamePad;
 var notifications;
 var modal;
+var timer;
 
 ///////////////////////////////////////////////////////////////////////////////
 
@@ -34,6 +35,9 @@ function init()
 	modal = new Modal();
 	modal.init();
 
+	timer = new Timer();
+	timer.init();
+
 	stateManager = new StateManager();
 	stateManager.init();
 
@@ -52,17 +56,30 @@ function setupDev()
 		// take off the prefix: admin-
 		var a = id.substring(6);
 		var parts = a.split("-");
-		var interrupt = false;
-		if ($(this).hasClass('interrupt'))
+
+		if ($(this).hasClass('state'))
 		{
-			interrupt = true;
+			var interrupt = false;
+			if ($(this).hasClass('interrupt'))
+			{
+				interrupt = true;
+			}
+			// fake a server publish event
+			PubSub.publish('event', {
+				group: parts[0],
+				state: parts[1],
+				interrupt: interrupt
+			});
 		}
-		// fake a server publish event
-		PubSub.publish('event', {
-			group: parts[0],
-			state: parts[1],
-			interrupt: interrupt
-		});
+
+		if ($(this).hasClass('timer'))
+		{
+			// fake a server publish timer
+			PubSub.publish('timer', {
+				event: parts[0]
+			});
+
+		}
 	});
 	$('.admin-event-bar').show();
 
