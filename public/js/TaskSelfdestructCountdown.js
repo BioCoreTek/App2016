9ad363969f76042a550035cb96dc4297fb5d6ab5
Annie Perrier <annieperrier@users.noreply.github.com>
2016-10-29 21:48:31 -0400

    unsubscribe on exit.

--- public/js/TaskSelfdestructCountdown.js
+++ public/js/TaskSelfdestructCountdown.js
@@ -3,6 +3,7 @@ function TaskSelfdestructCountdown()
 {
 	this.taskName = 'TaskSelfdestructCountdown';
 
+	this.pubSubs = [];
 };
 
 TaskSelfdestructCountdown.prototype.init = function()
@@ -12,17 +13,25 @@ TaskSelfdestructCountdown.prototype.init = function()
 	PubSub.publish('audio', {name: 'alarm', action: 'play', loop: true});
 
 	// listen for timer states
-	PubSub.subscribe('time', function(msg, data)
-	{
-		debug.debug('TaskSelfdestructCountdown PubSub sub time', msg, data);
-		self.processTimeEvent(data);
-	});
+	this.pubSubs.push(
+		PubSub.subscribe('time', function(msg, data)
+		{
+			debug.debug('TaskSelfdestructCountdown PubSub sub time', msg, data);
+			self.processTimeEvent(data);
+		});
+	);
 };
 
 // to be called when game exists
 TaskLifesupport.prototype.exit = function()
 {
 	PubSub.publish('audio', {name: 'alarm', action: 'stop'});
+
+	// remove pubsubs
+	for (var i = 0, len = this.pubSubs.length; i < len; i++)
+	{
+		PubSub.unsubscribe(this.pubSubs[i]);
+	}
 };
 
 TaskSelfdestructCountdown.prototype.processTimeEvent = function(data)
