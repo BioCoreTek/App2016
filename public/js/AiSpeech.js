e8650078075f6867453e4ffb5fb92a4e0c8cc1ef
Jeff Daze <jdaze@hostopia.com>
2016-10-30 19:41:11 -0400

    Added AI visualization #1

---
+++ public/js/AiSpeech.js
@@ -0,0 +1,45 @@
+/**
+ * Created by jeffdaze on 2016-10-30.
+ */
+//speech handlers...
+//init speechSynthesis API
+
+speechInit();
+
+function speechInit(){
+	speechSynthesis.onvoiceschanged = function() {
+		//these don't even need to be printed, just handled for the voices to be enumerated...
+		speechSynthesis.getVoices().forEach(function(voice, index) {
+			//console.log(voice, index);
+		});
+	};
+}
+
+//speech method...
+function say(text, callback)
+{
+
+	var msg = new SpeechSynthesisUtterance();
+	//var voices = window.speechSynthesis.getVoices();
+
+	var voices = speechSynthesis.getVoices();
+
+	msg.voice = voices[25];
+	//msg.rate = $('#rate').val() / 10;
+	//msg.pitch = $('#pitch').val();
+	msg.text = text;
+
+	//callback...
+	msg.onend = function(e) {
+
+		//for some reason this event needs to be handled or it won't process the callback...
+		console.log("e", e);
+		//console.log('Finished in ' + event.elapsedTime + ' seconds.');
+
+		//run the callback...
+		callback();
+
+	};
+
+	speechSynthesis.speak(msg);
+}
--- public/js/TaskAigoodDiagnostic.js
+++ public/js/TaskAigoodDiagnostic.js
@@ -4,17 +4,39 @@ function TaskAigoodDiagnostic()
 	this.taskName = 'TaskAigoodDiagnostic';
 
 	// length of time to wait to close modal in milliseconds
-	this.timeLengthContent = config.get('eventTimes')['TaskAigoodDiagnostic'];
+	//this.timeLengthContent = config.get('eventTimes')['TaskAigoodDiagnostic'];
 };
 
 TaskAigoodDiagnostic.prototype.init = function()
 {
 	var self = this;
 
+	//AI text
+	var AItext = "Welcome to the Aura, I have been offline for 4 days, 6 hours and 52 minutes. I require a system diagnostic. Please use the manual console until my return.";
+
+	//initialize and run the visualization...
+	//(AiVisuals.js)
+
+	//run the animation!
+	AIsetParams("good");
+	AIinit("AIContainer1");
+	AIanimate();
+
+
+	//make the AI speak (AiSpeech.js)
+	say(AItext, function(){
+		// just hide the modal when content is done
+		PubSub.publish('modal', {action: 'hide'});
+		// go to the life support screen
+		PubSub.publish('goToGroup', {group: 'lifesupport'});
+	});
+
+	/*
 	setTimeout(function() {
 		// just hide the modal when content is done
 		PubSub.publish('modal', {action: 'hide'});
 		// go to the life support screen
 		PubSub.publish('goToGroup', {group: 'lifesupport'});
 	}, this.timeLengthContent);
+	*/
 };
